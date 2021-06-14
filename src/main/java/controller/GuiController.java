ebd2ebb548cc153ab6f02b91a324113a85262a21
RodrigoOlman <58253035+RodrigoOlman@users.noreply.github.com>
2021-06-14 18:39:05 +0100

    main menu done, need improve settings and make buttons switch stages

--- src/main/java/controller/GuiController.java
+++ src/main/java/controller/GuiController.java
@@ -1,4 +1,22 @@
 package controller;
 
-public class GuiController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class GuiController implements Initializable {
+    public Button button;
+
+    public void settingsButton(){
+        System.out.println("entering settings...");
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("loading default settings");
+    }
 }
--- src/main/java/views/Main.java
+++ src/main/java/views/Main.java
@@ -2,19 +2,24 @@ package views;
 import controller.GuiController;
 import javafx.application.Application;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Gui.fxml"));
-
+        //this line doesnt work, cant find gui.fxml edit:needed to include fxml files to the resource directory, added in the pom file
+        //https://maven.apache.org/plugins/maven-resources-plugin/examples/include-exclude.html
+        Parent root = FXMLLoader.load(getClass().getResource("/Gui.fxml"));
+        primaryStage.setTitle("simulator");
+        primaryStage.setScene(new Scene(root,600,500));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
